f4b26e01d5a5e23d37d95ae6b700c135f217d1b7
caryljorosco <caryljohanan.orosco@gmail.com>
2016-12-21 21:28:32 +0800

    edited code, deleted photo

--- exercise6/CodeIgniter/application/views/news/index.php
+++ exercise6/CodeIgniter/application/views/news/index.php
@@ -58,7 +58,7 @@
 			<br>Favorite websites:
 			<br><a href= "https://www.youtube.com"><img border="0" alt="Youtube" src="<?php echo base_url('Images/youtube.jpg');?>" width=100 height=100></a>
 			<a href= "https://www.9gag.com"><img border="0" alt="9gag" src="<?php echo base_url('Images/9gag.jpg');?>" width=100 height=100></a>
-			<a href= "https://www.twitter.com"><img border="0" alt="Twitter" src="<?php echo base_url('Images/twitter.jpg');?>" width=100 height=100></a>
+			<a href= "https://www.youtube.com"><img border="0" alt="YouTube" src="<?php echo base_url('Images/youtube.jpg');?>" width=100 height=100></a>
 			</p>
 
 			<p>
