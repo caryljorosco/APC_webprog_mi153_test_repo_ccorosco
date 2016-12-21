38bccfe2214b66618e5c8cb0a5719c72fa1a1881
caryljorosco <caryljohanan.orosco@gmail.com>
2016-12-21 22:38:06 +0800

    edited code

--- exercise6/CodeIgniter/application/views/news/index.php
+++ exercise6/CodeIgniter/application/views/news/index.php
@@ -103,7 +103,7 @@
 				ANSWER
 			</p>
 
-			<button type="button" onclick="document.getElementById('demo4').innerHTML = 'Play on my PS3, or play basketball with friends.'">Click to see answer</button>
+			<button type="button" onclick="document.getElementById('demo4').innerHTML = ''My favorite is the case of JonBenet Ramsey (which had a 4-hour special at CBS and IT WAS AMAZING!)'">Click to see answer</button>
 
 			<p>
 				5. Where do you go to buy magic cards?
